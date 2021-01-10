54499ba536a8858a708b3c43dda2219ba6ce1e19
Nokealz <north.nuttapol@gmail.com>
2021-01-10 11:08:30 +0700

    add 3 files

---
+++ variables/multiple_word_name.py
@@ -0,0 +1,9 @@
+#Camel Case goldใช้อันนี้
+myVariableName = "Nuttapol Piromchaikit"
+
+#Pascal Case
+my Variable Name = "Nuttapol Piromchaikit"
+
+#Snake Case ส่วนใหญ่pythonใช้
+my_Variable_Name = "Nuttapol Piromchaikit"
+numbet_list = [1,2,3]
---
+++ variables/name.py
@@ -0,0 +1,13 @@
+#สามารถประกาศได้
+myvar = "North"
+my_var = "North"
+_my_var = "North"
+myvar = "North"
+MYVAR = "North"      #final ถ้าตัวแปรเป็นตัวใหญ่หมดจะไม่สามารถเปลี่ยนค่าได้
+myvar2 = "North"
+
+
+#ไม่สามารถประกาศได้
+2myvar = "North"
+my-var = "North"
+my var= "North"
