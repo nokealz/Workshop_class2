3a3e941020a7b4720ce535801550bfd4158bf0b5
Nokealz <north.nuttapol@gmail.com>
2021-01-16 11:50:05 +0700

    change folder

--- class2/string/Modify.py
+++ class2/string/Modify.py
@@ -2,6 +2,6 @@ string = " Hello, World! "
 print(string.upper())  # Output: " HELLO, WORLD!
 print(string.lower())  # Output: " hello, world!
 print(string.strip())  # Output: "Hello, World!"
-print(string.replace("H", "]"))  # Output: Jello, World!
+print(string.replace("H", "J"))  # Output: Jello, World!
 print(string.split(","))  # Output: [' Hello', ' World!]
 print(len(string))  # 15
